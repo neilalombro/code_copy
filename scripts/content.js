3129aaaa56f4282592801afce9be4f306b52819f
nfa44 <nfa44@uclive.ac.nz>
2023-08-28 23:16:49 +1200

    Fixed bug and version now copies the test in each cell.

--- scripts/content.js
+++ scripts/content.js
@@ -2,23 +2,32 @@ const coderunnerTables = document.getElementsByClassName("coderunnerexamples");
 
 if (coderunnerTables.length > 0) {
     Array.from(coderunnerTables).forEach(coderunnerTable => {
-        const copyButton = document.createElement("button");
-        copyButton.textContent = "Copy Test";
+        const testCells = coderunnerTable.querySelectorAll("tr td:first-child");
 
-        copyButton.addEventListener("click", function() {
-            const content = coderunnerTable.outerHTML;
+        testCells.forEach(cell => {
+            const copyButton = document.createElement("button");
+            copyButton.textContent = "Copy Test";
 
-            navigator.clipboard.writeText(content).then(() => {
-                const originalButtonText = copyButton.textContent;
-                copyButton.textContent = "Test Copied!";
-                setTimeout(function() {
-                    copyButton.textContent = originalButtonText;
-                }, 3000);
-            }).catch(err => {
-                console.error("Error copying test to clipboard:", err);
+            const content = cell.textContent;
+
+            copyButton.addEventListener("click", function(event) {
+                event.preventDefault();
+
+                navigator.clipboard.writeText(content).then(() => {
+                    const originalButtonText = copyButton.textContent;
+                    copyButton.textContent = "Test Copied!";
+                    setTimeout(function() {
+                        copyButton.textContent = originalButtonText;
+                    }, 3000);
+                }).catch(err => {
+                    console.error("Error copying test to clipboard:", err);
+                });
             });
-        });
 
-        coderunnerTable.parentNode.insertBefore(copyButton, coderunnerTable.nextSibling);
+            const breakParagraph = document.createElement("br");
+            
+            cell.appendChild(breakParagraph);
+            cell.appendChild(copyButton);
+        });
     });
 }
